86ddb77f01c70d0540cf55a6074f125970782988
afonia <afonderkin@gmail.com>
2013-12-23 10:35:09 +0700

    add pakege

---
+++ src/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: afonia
+ * Date: 12/23/13
+ * Time: 10:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
