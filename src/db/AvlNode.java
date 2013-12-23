0b8a3f5b74d3dd89e578e6052ef4063b2ec60773
salerat <salerat@gmail.com>
2013-12-23 13:49:05 +0700

    добавил авл структуру

---
+++ src/db/AvlNode.java
@@ -0,0 +1,19 @@
+package db;
+
+/** Here is the AVL-Node class for Completenesse **/
+public class AvlNode {
+    public AvlNode left;
+    public AvlNode right;
+    public AvlNode parent;
+    public int key;
+    public int balance;
+
+    public AvlNode(int k) {
+        left = right = parent = null;
+        balance = 0;
+        key = k;
+    }
+    public String toString() {
+        return "" + key;
+    }
+}
