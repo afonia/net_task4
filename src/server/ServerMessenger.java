f43f408dfe3c9641dd28dddcb7d12515ae43afc3
afonia <afonderkin@gmail.com>
2013-12-23 12:59:16 +0700

    добавил Заглушки

--- src/server/ServerMessenger.java
+++ src/server/ServerMessenger.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,9 +20,65 @@ public class ServerMessenger implements Runnable {
     private Socket fromClient = null;
     private BufferedReader in = null;
     private PrintWriter out = null;
+    private boolean exit = false;
+
+    public ServerMessenger(){
+        try {
+            System.out.println("Messenger start init");
+            serverSocket = new ServerSocket(port);
+            fromClient = serverSocket.accept();
+            in = new BufferedReader(new InputStreamReader(fromClient.getInputStream()));
+            out = new PrintWriter(fromClient.getOutputStream(),true);
+
+            System.out.println("Messenger inited");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dispose(){
+        try {
+            out.close();
+            in.close();
+            fromClient.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void scan(){
+
+    }
+
+    public void checkChildrens(){
+
+    }
+
+    public void checkParent(){
+
+    }
+
+    public void scanForAtherServerInSubnet(){
+
+    }
 
     @Override
     public void run() {
+        System.out.println("Messenger Start");
+
+        while (!exit){
+            try {
+                System.out.println("Messenger work");
+                Thread.sleep(1000);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
