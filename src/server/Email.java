b68a0cd9c636a30cc8668345f000b67c193cd83d
afonia <afonderkin@gmail.com>
2013-12-26 05:57:03 +0700

    send messeges

---
+++ src/server/Email.java
@@ -0,0 +1,14 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: afonia
+ * Date: 12/26/13
+ * Time: 5:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Email {
+    public String adress;
+    public String messege;
+
+}
---
+++ src/server/Server.java
@@ -0,0 +1,14 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: afonia
+ * Date: 12/26/13
+ * Time: 5:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+    public String fromEmailAdress = "seraga-afon@mail.ru";
+    public String pass = "rtyujh";
+
+}
